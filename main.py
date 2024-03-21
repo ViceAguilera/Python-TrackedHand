c52da44eb35fbd97a4d7b18c49b8171bf834cf76
vicente.aguilera1908@gmail.com <vicente.aguilera2001@alumnos.ubiobio.cl>
2024-03-21 00:53:54 -0300

    Actualizacion

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
-import socket
 
-#parametro
-width, height = 1280, 720
-#Webcam
-cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
-#Dectector de manos
-detector2 = HandDetector(detectionCon=0.8)
-#comunicacion
-com = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverAddress = ('127.0.0.1', 5052)
 
-while True:
-    success, img = cap.read()
-    hands, img = detector2.findHands(img)
-    dataBody=[]
-    dataHands=[]
-    if hands:
-        hand = hands[0]
-        lmListHands = hand["lmList"]
-        for lm in lmListHands:
-            dataHands.extend([lm[0], height-lm[1], lm[2]])
-        print(dataHands)
+def main():
+    width, height = 1280, 720
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1280)
+    cap.set(4, 720)
+    detector = HandDetector(detectionCon=0.8)
 
-    com.sendto(str.encode(str(dataHands)), serverAddress)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        hands, img = detector.findHands(frame)
 
-    img = cv2.resize(img,(0,0),None,0.5,0.5)
-    cv2.imshow("Video", img)
-    cv2.waitKey(1)
+        if hands:
+            hand = hands[0]
+            fingers = detector.fingersUp(hand)
+            dataFingers = fingers
+            print(dataFingers)
+
+        img = cv2.resize(img,(0,0),None,0.5,0.5)
+        cv2.imshow("Video", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
